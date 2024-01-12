20e3892d76d47a375e89a21e9a45816b8efe9460
fidaatag <fidaa2fidaa@gmail.com>
2024-01-12 09:09:06 +0700

    fix railway : change bodyparser to express

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const todoRouter = require('./routes/todoRoutes')
 const bodyParser = require('body-parser')
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // db connection
 connecToDB()
